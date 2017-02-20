5708ffda7ca67d9c50ece5724d1c28646a7c33c0
xavierouicar <xavier@ouicar.fr>
2017-02-20 17:30:55 +0100

    add dependency for material-ui touch actions

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -13,12 +13,17 @@
 
 import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
 export default class App extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
 
   static propTypes = {
     children: React.PropTypes.node,
   };
+
+  componentDidMount() {
+    injectTapEventPlugin();
+  }
 
   render() {
     return (
